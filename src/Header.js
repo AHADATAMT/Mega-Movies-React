1ac9d56a86fb5b2d15b4e101023f50509ca5114c
M.T <bmthang94@gmail.com>
2019-03-24 20:15:06 +0700

    search button

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 export default class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: ''
+    }
+  }
+  handleSearchInput = e => {
+    let value = e.target.value;
+    console.log(value);
+    this.setState({
+      searchString: value
+    })
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark indigo mb-4">
-          <Link to={'/'}><h3 className="navbar-brand">MEGA MOVIE</h3></Link> 
+          <Link to={'/'}><h3 className="navbar-brand">MEGA MOVIE</h3></Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <form className="form-inline ml-auto">
               <div className="md-form my-0">
-                <input id="searchInput" className="form-control" type="text" placeholder="Search" aria-label="Search" />
+                <input id="searchInput" className="form-control" type="text" placeholder="Search" aria-label="Search" onChange={this.handleSearchInput} />
               </div>
-              <button id="searchBtn" className="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">Search</button>
+              <SearchBtn searchString={this.state.searchString} />
             </form>
           </div>
         </nav>
@@ -20,3 +35,15 @@ export default class Header extends Component {
     )
   }
 }
+const SearchBtn = (props) => (
+  <Route render={({ history }) => (
+    <button
+      id="searchBtn"
+      className="btn btn-outline-white btn-md my-0 ml-sm-2"
+      type='button'
+      onClick={() => { history.push('/search/q=' + props.searchString) }}
+    >
+      Search
+    </button>
+  )} />
+)
